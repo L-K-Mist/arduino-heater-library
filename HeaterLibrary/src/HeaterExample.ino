66212bd776e0cf4216ee3059503bee637c1b9e7b
L-K-Mist <dylan@vandenbosch.co.za>
2016-10-04 16:54:16 +0200

    Update and rename HeaterExample.cpp to HeaterExample.ino

--- HeaterLibrary/src/HeaterExample.ino
+++ HeaterLibrary/src/HeaterExample.ino
@@ -9,7 +9,7 @@ Flasher hotFlasher_PM(21); //input the digital pin that you want to flash some k
 Flasher fanFlasher_PM(20); //input the digital pin that you want to use for cooling.  When the Target Temp is close or surpassed this pin goes HIGH to prevent over-shooting.
 Thermistor pm(A0); //input the analogue pin for your thermistor. This creates the Thermistor object for heaterOne to use.
 TempSensor *tMistor_PM = &pm; //to allow you to choose whether you want your heater to use a thermistor or thermocouple we need some pointers. In the next line we put tMistor_PM into heaterOne.
-Heater heaterOne("Plastic Melting", hotFlasher_PM, fanFlasher_PM, tMistor_PM);
+Heater heaterOne("Heater One", hotFlasher_PM, fanFlasher_PM, tMistor_PM);
 //Notice above that the Flasher object named allocationObj_Flasher is inserted into Heater object named pm, by calling it's name!!!
 
 //The result:  every time we need a new Heater Object for a particular zone, we just give it a new name and attributes.
@@ -19,26 +19,27 @@ Heater heaterOne("Plastic Melting", hotFlasher_PM, fanFlasher_PM, tMistor_PM);
 //=============================
 Flasher hotFlasher_RC(19);
 Flasher fanFlasher_RC(18);
-Thermocouple rc(50,52,44,46,48); //input your thermocouple pins as follows: vccPin, gndPin, sclk, cs, miso - You can put all your pins in a line. The thermocouple code will make any given digital pin into gnd and vcc.
+Thermocouple rc(50,52,44,46,48); //input your thermocouple pins as follows: vccPin, gndPin, sclk, cs, miso - physically you can put all your pins in a line. The thermocouple code will make any given digital pin into gnd and vcc.
 
 TempSensor *tCouple_RC = &rc; //notice that this time we are able to put a thermocouple object into heaterTwo; instead of a thermistor as in heaterOne.
-Heater heaterTwo("Reaction Chamber", hotFlasher_RC, fanFlasher_RC, tCouple_RC);
-//------------------end of Reaction Chamber instantiation
+Heater heaterTwo("Heater Two", hotFlasher_RC, fanFlasher_RC, tCouple_RC);
+//------------------end of heaterTwo instantiation
+
+//If you require more heaters just copy and tweak one of the above instantiation examples.
 
 
 void setup(){
   Serial.begin(9600);
-  pinMode(14,OUTPUT); //in my case this code is just here to make pin 14 into a groundpin
-  digitalWrite(14, LOW); //in my case this code is just here to make pin 14 into a groundpin
+  pinMode(14,OUTPUT); digitalWrite(14, LOW); //in my case this code is just here to make pin 14 into a groundpin
 
   heaterOne.setup(); //sets up the required pins
   heaterOne.printInfo(); // optional (more for debugging)
-  heaterOne.setMinMaxTemp(5, 25); // set up the operating range for the heater. Think room-temp to Max operating temp.
+  heaterOne.setMinMaxTemp(5, 45); // set up the operating range for the heater. Think room-temp to Max operating temp. Here it is set to 45 deg C for testing with a coffee-cup
   heaterOne.setTargetTemp(20); // set target temp in C. Probably the most important function. In my application this function will be used repeatedly to set new target temps for each heater.
 
   heaterTwo.setup();
   heaterTwo.printInfo(); // optional (more for debugging)
-  heaterTwo.setMinMaxTemp(5, 8);
+  heaterTwo.setMinMaxTemp(20, 60);
   heaterTwo.setTargetTemp(23);
  }
 
